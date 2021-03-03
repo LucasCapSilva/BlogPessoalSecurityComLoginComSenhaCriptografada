ca27377538d543b5a44cd484e7022ced2b298985
Lucas Capelotto da silva <lcapelotto@gmail.com>
2021-03-02 23:11:20 -0300

    1.0

--- blogPessoal-api-master/src/main/java/generation/org/blogPessoal/controller/UsuarioController.java
+++ blogPessoal-api-master/src/main/java/generation/org/blogPessoal/controller/UsuarioController.java
@@ -8,9 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,11 +40,6 @@ public class UsuarioController {
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
-	}
-
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
 		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
@@ -56,11 +49,6 @@ public class UsuarioController {
 			return ResponseEntity.badRequest().build();
 		}
 		
-	}
-
-	@PutMapping
-	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario) {
-		return ResponseEntity.ok(repository.save(usuario));
 	}
 
 }
